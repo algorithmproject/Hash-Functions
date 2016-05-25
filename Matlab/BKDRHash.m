1062c4d4df30486b6a342c734a4e6507fd1cfaf6
Sixun Ouyang <troy.oysx@gmail.com>
2016-05-25 12:15:49 +0100

    Create BKDRHash.m

---
+++ Matlab/BKDRHash.m
@@ -0,0 +1,13 @@
+%% a hash function from the book named "The C Programming Language"
+% param:
+%   tar, the product to be given a hash code
+%   m, the length of hash vector
+function hash = BKDRHash(tar, m)
+hash = 1;
+tar = int2str(tar);
+for i = tar,
+    % bitsll : shift left
+    % bitsra : shift right
+    hash = bitsll(hash, 7) + bitsll(hash, 1) + hash + str2double(i);
+end
+hash = int32(hash);
