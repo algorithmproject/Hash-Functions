5787978df0055749d80a2c60577d0772c6116d01
Sixun Ouyang <troy.oysx@gmail.com>
2016-05-25 12:12:14 +0100

    Create RSHash.m

---
+++ RSHash.m
@@ -0,0 +1,14 @@
+%% simple hash function from Robert Sedgwicks Algorithm
+% param:
+%   tar, the product to be given a hash code
+%   m, the length of hash vector
+function hash = RSHash(tar, m)
+b = 3;
+a = 2;
+hash = 1;
+tar = int2str(tar);
+for i = tar,
+    hash = hash * a + str2double(i);
+    a = a * b;
+end
+hash = int32(hash);
